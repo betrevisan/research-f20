79dee3e959cc7cfc96d67ee4f156a03e9439571b
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2020-11-19 22:11:40 -0300

    outsource every worker

--- always_outsource.py
+++ always_outsource.py
@@ -7,24 +7,28 @@ workers_file = '/Users/bernardoetrevisan/Desktop/research-f20/workers.tsv'
 def main():
     skills = load_skills() # Load skills into a list.
     workers = load_workers() # Load workers into a list.
-    print(workers[0])
     p = 5 # Value of p is defined as 5 for now.
     history = [] # List that keeps track of requests
 
     # Initialize the skill to be a random one.
     curr_skill = random_skill(skills)
+    # Find worker with the given skill and add them to the cache
+    cache = find_worker(workers, curr_skill)
+    # Outsource worker
+    cost = float(cache[4])
     history.append(curr_skill)
-    # cache = find_worker()
 
     # Stream of 1K requests.
     for i in range(1000):
         if random.random() <= 1/p:
             curr_skill = random_skill(skills)
-            #cache = find_worker()
-        
+            cache = find_worker(workers, curr_skill)
+
+        # Outsource worker
+        cost = cost + float(cache[4])
         history.append(curr_skill)
 
-        # Outsource worker with this given skill
+    print("The total cost was $" + str(format(cost, '.2f')))
 
 # Helper function that reads the skills tsv file and stores it into an output list.     
 def load_skills():
@@ -42,7 +46,14 @@ def load_workers():
 
 # Helper function that returns a random skill from all the possible ones.
 def random_skill(skills):
-    return random.choice(skills)
+    return random.choice(skills)[0]
+
+# Helper function that finds the worker with the given skill id and returns them.
+def find_worker(workers, skill_id):
+    for worker in workers:
+        if worker[7] == skill_id:
+            return worker
+    return False
 
 
 
