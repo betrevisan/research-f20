7e0ba91005a30b90ad693958317c5c5a34795a05
bernardoetrevisan <bernardo.trevisan@rocket.chat>
2020-11-20 10:10:09 -0300

    write the always hire function

--- always_outsource.py
+++ always_outsource.py
@@ -7,11 +7,13 @@ workers_file = '/Users/bernardoetrevisan/Desktop/research-f20/workers.tsv'
 def main():
     skills = load_skills() # Load skills into a list.
     workers = load_workers() # Load workers into a list.
-    stream = random_stream(skills)
+    stream = random_stream(skills) # Generates the random stream of requests.
 
     cost_outsource = always_outsource(stream, workers)
+    cost_hire = always_hire(stream, workers)
 
     print("The cost of always outsourcing was $" + str(format(cost_outsource, '.2f')))
+    print("The cost of always hiring was $" + str(format(cost_hire, '.2f')))
 
 # Generates a random stream of requests to be used consistently across the different algorithms.
 def random_stream(skills):
@@ -30,6 +32,7 @@ def random_stream(skills):
     
     return stream
 
+# Algorithm that always outsources workers and keeps the cache empty
 def always_outsource(stream, workers):
     cost = 0
 
@@ -44,8 +47,21 @@ def always_outsource(stream, workers):
     
     return cost
 
+# Algorithm that always hires workers when they are not in the cache
+def always_hire(stream, workers):
+    cost = 0
+    cache = [None, None, None, None, None, None, None, None]
 
-
+    # For each request in the stream, outsource
+    for r in stream:
+        # Hire onlye if the worker is not yet in the cache.
+        if r != cache[7]:
+            # Find worker with the given skill and add them to the cache.
+            cache = find_worker(workers, r)
+            # Hire worker and keep them in the cache.
+            cost = cost + float(cache[3])
+    
+    return cost
 
 # Helper function that reads the skills tsv file and stores it into an output list.     
 def load_skills():
